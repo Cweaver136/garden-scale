025e9bbb953314a73ef824c4f01dced47f95d494
Caleb Weaver <cweaver136@gmail.com>
2025-06-13 13:59:33 -0400

    added rollup

---
+++ rollup.config.js
@@ -0,0 +1,43 @@
+import { rollupPluginHTML as html } from '@web/rollup-plugin-html';
+import nodeResolve from '@rollup/plugin-node-resolve';
+
+const pagesArray = [
+];
+
+const pagesToInclude = pagesArray?.map((page) => `${page}/**/*.html`) || [];
+pagesToInclude.push('index.html');
+
+const CONFIG = {
+  // our input files, relative reference from rootDir specified below
+  input: [...pagesToInclude],
+  // plugins to make the thing go zoom zoom
+  plugins: [
+    // plugin for generating HTML files from rollup
+    html({
+      // keep our file structure intact
+      flattenOutput: false,
+      // the root directory to start at
+      rootDir: 'source',
+      // keep non-module assets
+      extractAssets: true,
+    }),
+    // module resolution
+    nodeResolve({ browser: true }),
+    // copy dependencies that we cannot or do not want to bundle
+  ],
+  preserveEntrySignatures: false,
+  output: {
+    // the output directory for the code
+    dir: 'compiled',
+    // module output format
+    format: 'esm',
+    // name scheme for entry chunks ([hash] is an option, but we're opting for name only to avoid cache problems)
+    entryFileNames: '[name].js',
+    // name scheme for shared chunks
+    chunkFileNames: '[name].js',
+    // include sourcemaps
+    sourcemap: true
+  }
+};
+
+export default  [ CONFIG ];
